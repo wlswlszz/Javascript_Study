7c0b4340c7b7d9344fe2e4d475a11e8e9836e1c1
wlswlszz <wlswlszz1234@gmail.com>
2023-09-06 17:21:21 +0900

    함수의 대한 이해

--- html_data/test01-01.js
+++ html_data/test01-01.js
@@ -2,24 +2,17 @@
 
 
 
-// let a;
-// a ="abc";
-// {
-//     let a;
-//     a = 5;
+let a;
+a ="abc";
+{
+    let a;
+    a = 5;
 
-// }
-// document.write(a);
+}
+document.write(a);
 
 
-let dec = prompt("숫자를 입력해 주세요","");
-let bin = dec.toString(2);
-let oct = dec.toString(8);
-let hex = dec.toString(16);
-prompt(bin);
-document.write(bin);
-document.write(oct);
-document.write(hex);
+
 
 
 
